fe6072e0824c009780d0a70e72b44a6c60ab78c9
ubuntunux <ubuntunux@gmail.com>
2023-12-14 10:36:03 +0900

    particle

--- game/effect.py
+++ game/effect.py
@@ -21,21 +21,28 @@ class FxManager(EffectManager):
     def create_effects(self, player):
         game_resource = GameResourceManager.instance()
         particle_info = dict(
+            is_world_space=True,
             loop=-1,
             fade=1,
             color=(1,1,1,1),
-            collision=True,
             texture=game_resource.get_image("explosion").texture,
             delay=RangeVar(0.0,1.0), 
             angular_velocity=RangeVar(360.0), 
             rotate=RangeVar(0.0, 360), 
             offset=RangeVar((-20,20), (-20,20)),
-            life_time=RangeVar(5.5,5.5), 
+            life_time=RangeVar(0.5,1.5), 
             sequence=[4,4],
-            velocity=RangeVar([-2000.0, 100], [2000.0, 200]),
-            gravity=RangeVar(900.0)
+            velocity=RangeVar([-200.0, 100], [200.0, 200]),
+            gravity=RangeVar(0.0)
         )
-        self.create_emitter('explosion', particle_info, 20, pos=(50,50), size=(100,100))
+        self.create_emitter(
+            emitter_name='explosion',
+            attach_to=player,
+            pos=(50,50),
+            size=(100,100),
+            particle_info=particle_info,
+            particle_count=20
+        )
         effect = self.get_emitter('explosion')
-        effect.play_particle(None, True)
+        effect.play()
         
