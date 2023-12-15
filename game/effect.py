f400701a499bc9a3ec659363f48681192a35ff3b
ubuntunux <ubuntunux@gmail.com>
2023-12-15 18:32:01 +0900

    particle data

--- game/effect.py
+++ game/effect.py
@@ -1,6 +1,6 @@
 from kivy.logger import Logger
 from kivy.vector import Vector
-from utility.particle import EffectManager, Particle
+from utility.particle import EffectManager
 from utility.singleton import SingletonInstance
 from utility.kivy_helper import *
 from utility.range_variable import RangeVar
@@ -20,29 +20,13 @@ class FxManager(EffectManager):
         
     def create_effects(self, player):
         game_resource = GameResourceManager.instance()
-        particle_info = dict(
-            is_world_space=False,
-            loop=-1,
-            fade=1,
-            color=(1,1,1,1),
-            texture=game_resource.get_image("explosion").texture,
-            delay=RangeVar(0.0,1.0), 
-            angular_velocity=RangeVar(360.0), 
-            rotate=RangeVar(0.0, 360), 
-            offset=RangeVar((-20,20), (-20,20)),
-            life_time=RangeVar(0.5,1.5), 
-            sequence=[4,4],
-            velocity=RangeVar([-200.0, 100], [200.0, 200]),
-            gravity=RangeVar(0.0)
-        )
-        self.create_emitter(
+        particle_data = game_resource.get_particle_data("explosion")
+        emitter = self.create_emitter(
             emitter_name='explosion',
             attach_to=None,
             pos=(50,50),
             size=(100,100),
-            particle_info=particle_info,
+            particle_data=particle_data.get_data(),
             particle_count=20
         )
-        effect = self.get_emitter('explosion')
-        effect.play()
-        
+        emitter.play()
--- game/game_resource.py
+++ game/game_resource.py
@@ -1,12 +1,12 @@
 import os
 from kivy.logger import Logger
-from kivy.uix.image import Image
 from utility.singleton import SingletonInstance
 from utility.resource import ResourceManager
 from .character import CharacterData
 from .tile import TileData, TileDataSet
 
 game_path = "KivyRPG"
+effects_path = os.path.join(game_path, "data/effects")
 images_path = os.path.join(game_path, "data/images")
 maps_path = os.path.join(game_path, "data/maps")
 tile_data_path = os.path.join(game_path, "data/tiles")
@@ -16,21 +16,14 @@ character_data_path = os.path.join(game_path, "data/characters")
 class GameResourceManager(ResourceManager):
     def __init__(self):
         super(GameResourceManager, self).__init__()
-        self.images = {}
         self.tile_data_set = {}
         self.character_data = {}
     
     def initialize(self):
         self.register_resources(images_path, [".png", ".jpg"], self.images, self.image_loader)
+        self.register_resources(effects_path, [".data"], self.particle_data, self.particle_data_loader)
         self.register_resources(tile_data_path, [".data"], self.tile_data_set, self.tile_data_loader)
         self.register_resources(character_data_path, [".data"], self.character_data, self.character_data_loader)
-    
-    # image
-    def get_image(self, resource_name):
-        return self.get_resource(self.images, resource_name)
-        
-    def image_loader(self, name, filepath):
-        return Image(source=filepath)
     
     # tile
     def get_tile_data_set(self, resource_name):
